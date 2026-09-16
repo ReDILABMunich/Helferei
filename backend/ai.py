8d19a425c9351ed5a091d3b0d81565e63de74c1a
Basant Morsi <basant179@gmail.com>
2026-09-16 12:53:40 +0200

    changed ai used from groq to openai, chat gpt 4 mini

--- backend/ai.py
+++ backend/ai.py
@@ -1,23 +1,37 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
+from openai import OpenAI
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.environ["GROQ_API_KEY"]
+# Client for groq
+# client = Groq(
+#     api_key=os.environ["GROQ_API_KEY"]
+# )
+
+client = OpenAI(
+    api_key=os.environ["OPENAI_API_KEY"]
 )
 
+# Function for groq ai
+# def askAI(prompt: str) -> str:
+#     response = client.chat.completions.create(
+#         model="openai/gpt-oss-20b",
+#         messages=[
+#             {
+#                 "role": "user",
+#                 "content": prompt,
+#             }
+#         ],
+#     )
+#
+#     return response.choices[0].message.content
 
 def askAI(prompt: str) -> str:
-    response = client.chat.completions.create(
-        model="openai/gpt-oss-20b",
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=prompt,
     )
 
-    return response.choices[0].message.content
+    return response.output_text
